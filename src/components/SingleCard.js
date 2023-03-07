fbeb78a0ca783c194dd99c87aa8dd932912ad472
AryanVik <99641342+AryanVik@users.noreply.github.com>
2023-03-08 00:07:18 +0530

    first commit

---
+++ src/components/SingleCard.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./SingleCard.css";
+import { cover} from '../images';
+// const backGroundImage = [
+//     {src:cover}
+// ]
+
+const SingleCard = ({ card,handleChoice, flipped,disabled }) => {
+    const handleClick = () =>{
+        if(!disabled){
+            handleChoice(card)
+        }
+    }
+  return (
+    <div className="card">
+      <div className={flipped ? "flipped" : " "}>
+        <img className="front" src={card.src} alt="card-front" />
+        <img 
+            className="back" 
+            src={cover}
+            onClick={handleClick}
+            alt="card-back" 
+        />
+      </div>
+    </div>
+  );
+};
+
+export default SingleCard;
---
+++ src/images.js
@@ -0,0 +1,7 @@
+export const helmet = require('./images/helmet-1.png');
+export const potion = require('./images/potion-1.png');
+export const ring = require('./images/ring-1.png');
+export const scroll = require('./images/scroll-1.png');
+export const shield = require('./images/shield-1.png');
+export const sword = require('./images/sword-1.png');
+export const cover = require('./images/cover.png');
